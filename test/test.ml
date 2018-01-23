e1dd2c2029d784251d623c2e7bffc285bd9d5978
Alessandro Strada <alessandro.strada@gmail.com>
2018-01-23 22:36:28 +0100

    Add alcotest

--- test/test.ml
+++ test/test.ml
@@ -1,11 +1,35 @@
-let stylesheet = [%css "{
+
+let test_stylesheet_parser () =
+  let css = "{
   /* This is a comment */
   color: red; /* This is another comment */
   backgroun-color: red;
 }
 
 p {
-}"]
+}"
+  in
+  let ast = Ppx_bs_css.(Css_lexer.parse_string css Css_parser.stylesheet) in
+  let expected = " {
+\tcolor: red;
+\tbackgroun-color: red
+}
+
+p {
+\t
+}
+
+" in
+  Alcotest.(check string)
+    "different CSS content"
+    expected
+    (Ppx_bs_css.Css_printer.stylesheet_to_string ast)
+
+let test_set = [
+  "CSS parser", `Quick, test_stylesheet_parser;
+]
 
 let () =
-  print_endline stylesheet
+  Alcotest.run "Test suite" [
+    "test_set", test_set;
+  ]
