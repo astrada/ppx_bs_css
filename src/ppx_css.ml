0132e833a6d09e4225e1c353b2d6cf338b1e6ddf
Alessandro Strada <alessandro.strada@gmail.com>
2018-03-04 11:30:19 +0100

    Add %style extension point
    Use %css to parse stylesheets, %style to parse declaration lists

--- src/ppx_css.ml
+++ src/ppx_css.ml
@@ -8,31 +8,47 @@ open Longident
 
 let rec expr mapper e =
   match e.pexp_desc with
-  | Pexp_extension ({ txt = "css"; loc}, PStr [{ pstr_desc = Pstr_eval (e, _) }])
-    -> (match e.pexp_desc with
-        | Pexp_constant Pconst_string (str, delim) ->
-          let loc_start =
-            match delim with
-            | None -> e.pexp_loc.Location.loc_start
-            | Some s ->
-              { e.pexp_loc.Location.loc_start with
-                Lexing.pos_cnum =
-                  e.pexp_loc.Location.loc_start.Lexing.pos_cnum + (String.length s) + 1
-              }
-            in
-          let ast =
-            Css_lexer.parse_string
-              ~container_lnum:loc_start.Lexing.pos_lnum
-              ~pos:loc_start
-              str
-              Css_parser.declaration_list in
-          Css_to_ocaml.render_declaration_list ast
-        | _ ->
-          raise
-            (Location.Error
-               (Location.error ~loc
-                  "[%css] accepts a string, e.g. [%css \"{\n  color: red;\n}\"]"))
-      )
+  | Pexp_extension
+      ({ txt; loc},
+       PStr [{ pstr_desc = Pstr_eval (e, _) }]) when txt = "style" || txt = "css" ->
+    begin match e.pexp_desc with
+      | Pexp_constant Pconst_string (str, delim) ->
+        let loc_start =
+          match delim with
+          | None -> e.pexp_loc.Location.loc_start
+          | Some s ->
+            { e.pexp_loc.Location.loc_start with
+              Lexing.pos_cnum =
+                e.pexp_loc.Location.loc_start.Lexing.pos_cnum + (String.length s) + 1
+            }
+        in
+        let container_lnum = loc_start.Lexing.pos_lnum in
+        let pos = loc_start in
+        begin match txt with
+          | "style" ->
+            let ast =
+              Css_lexer.parse_string
+                ~container_lnum
+                ~pos
+                str
+                Css_parser.declaration_list in
+            Css_to_ocaml.render_declaration_list ast
+          | "css" -> 
+            let ast =
+              Css_lexer.parse_string
+                ~container_lnum
+                ~pos
+                str
+                Css_parser.stylesheet in
+            Css_to_ocaml.render_stylesheet ast
+          | _ -> assert false
+        end
+      | _ ->
+        raise
+          (Location.Error
+             (Location.error ~loc
+                "[%css] accepts a string, e.g. [%css \"{\n  color: red;\n}\"]"))
+    end
   | _ -> default_mapper.expr mapper e
 
 
