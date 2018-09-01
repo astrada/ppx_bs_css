886c41dc14eab98d45794b93cb50095191da758d
Alessandro Strada <alessandro.strada@gmail.com>
2018-09-01 18:43:11 +0200

    Add tests

---
+++ test_bs/issues/issue_6.ml
@@ -0,0 +1,41 @@
+open TypedGlamor
+
+let test = [%style {typed|
+  border-top-right-radius: 1px;
+  border-top-right-radius: 1px 2px;
+  border-top-left-radius: 1px;
+  border-top-left-radius: 1px 2px;
+  border-bottom-right-radius: 1px;
+  border-bottom-right-radius: 1px 2px;
+  border-bottom-left-radius: 1px;
+  border-bottom-left-radius: 1px 2px;
+
+  background-position: initial;
+  background-position: 1px 2px;
+
+  transform-origin: initial;
+  transform-origin: 1px 2px;
+
+  flex: 1.0 2.0 100px;
+|typed}]
+
+let equal = [
+  borderTopRightRadius (px 1);
+  borderTopRightRadius2 ~v:(px 1) ~h:(px 2);
+  borderTopLeftRadius (px 1);
+  borderTopLeftRadius2 ~v:(px 1) ~h:(px 2);
+  borderBottomRightRadius (px 1);
+  borderBottomRightRadius2 ~v:(px 1) ~h:(px 2);
+  borderBottomLeftRadius (px 1);
+  borderBottomLeftRadius2 ~v:(px 1) ~h:(px 2);
+
+  backgroundPosition initial;
+  backgroundPosition2 ~h:(px 1) ~v:(px 2);
+
+  transformOrigin initial;
+  transformOrigin2 ~h:(px 1) ~v:(px 2);
+
+  flex3 ~grow:1.0 ~shrink:2.0 (px 100);
+]
+
+let _ = assert (test = equal)
