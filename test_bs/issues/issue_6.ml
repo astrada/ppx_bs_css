666c5faff3484ad2c7cb4d924564703605cfc8c1
Alessandro Strada <alessandro.strada@gmail.com>
2018-09-06 21:34:53 +0200

    Fix #6
    Add shadow/box-shadow special casing for bs-typed-glamor

--- test_bs/issues/issue_6.ml
+++ test_bs/issues/issue_6.ml
@@ -20,6 +20,9 @@ let test = [%style {typed|
 
   outline: solid 100px;
   border: red dotted;
+
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3), inset 0 1px 10px rgba(255, 255, 255, 0.5);
 |typed}]
 
 let equal = [
@@ -42,6 +45,14 @@ let equal = [
 
   outline2 ~width:(px 100) solid;
   border2 ~color:red dotted;
+
+  boxShadows([
+      shadow ~x:zero ~y:(px 1) ~blur:(px 5) (rgba 0 0 0 0.3);
+    ]);
+  boxShadows([
+      shadow ~x:zero ~y:(px 1) ~blur:(px 5) (rgba 0 0 0 0.3);
+      shadow ~x:zero ~y:(px 1) ~blur:(px 10) ~inset:true (rgba 255 255 255 0.5);
+    ]);
 ]
 
 let _ = assert (test = equal)
